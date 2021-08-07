381ee633a8b314386007b090d61035fdfdec743d
Pawan kr <itsmekr.info@gmail.com>
2021-08-07 09:34:51 +0530

    change in css and add nextSlide and prevSlide functionality

--- src/App.js
+++ src/App.js
@@ -2,18 +2,46 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-  const [name, setName] = useState('')
+  const [current, setCUrrent] = useState(0)
+  const arr = ['red', 'green', 'orange'] //total number of slider
 
+  const nextSlide = () => {
+    //set current value with condition when current is "last slide" then set the current value "fist slide"
+    setCUrrent(current === arr.length - 1 ? 0 : current + 1)
+  }
+
+  const prevSlide = () => {
+    //set current value with condition when current is "fist slide" then set the current value "last slide"
+    setCUrrent(current === 0 ? arr.length - 1 : current - 1)
+  }
+
+  setInterval(() => {}, 3000)
+
+  console.log(current)
   return (
     <div className="App">
       <div className="slide">
-        {[1].map(() => (
-          <div className="slide_item"></div>
+        {arr.map((data, index) => (
+          <div
+            className={index === current ? 'active_slide' : 'de_active_slide'}
+          >
+            {/* render div with current value */}
+
+            {index === current && (
+              <div
+                style={{ backgroundColor: data }}
+                key={index}
+                className="slide_item"
+              >
+                {index + 1}
+              </div>
+            )}
+          </div>
         ))}
 
         <div className="next_prev">
-          <button> prev </button>
-          <button> next </button>
+          <button onClick={prevSlide}>prev</button>
+          <button onClick={nextSlide}>next</button>
         </div>
       </div>
     </div>
